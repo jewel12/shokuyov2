2371290cfbeecdda1582a49b38552428f41ccc68
jewel12 <xiedajeeling@gmail.com>
2023-12-22 01:45:53 +0900

    Add htmx animation for image scaling

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import type { FC } from "hono/jsx";
+import { html } from "hono/html";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import OpenAI from "openai";
@@ -17,6 +18,19 @@ const Top: FC = () => {
           crossorigin="anonymous"
         ></script>
         <script src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
+        {html`
+          <script>
+            htmx.on("htmx:load", function (e) {
+              if (e.srcElement.id == "syokuyo") {
+                setInterval(function () {
+                  scales = [-1, 1];
+                  i = Math.floor(2 * Math.random());
+                  e.srcElement.style.transform = "scale(" + scales[i] + ", 1)";
+                }, 70);
+              }
+            });
+          </script>
+        `}
       </head>
       <body
         hx-ext="response-targets"
@@ -29,7 +43,9 @@ const Top: FC = () => {
           <input type="text" name="name" />
           <input type="submit" value="食用" />
         </form>
-        <div id="result"></div>
+        <div id="result">
+          <p class="htmx-indicator">loading...</p>
+        </div>
       </body>
     </html>
   );
@@ -42,12 +58,17 @@ const Shokuyo: FC<{
 }> = (props: { name: string; edible: boolean; imageUrl: string }) => {
   return (
     <>
-      <img src={props.imageUrl} style={{ width: 256 }} />
+      <img
+        id={props.edible ? "syokuyo" : "tabenaide"}
+        src={props.imageUrl}
+        style={{ width: 256 }}
+      />
       <div>
         <b>
           {props.name}は{props.edible ? "食べられます！" : "食べられません"}
         </b>
       </div>
+      <p>画像が出るまでちょっと待ってね。</p>
       <p>判定結果は間違っていることががあります。</p>
       <p>食べて健康を害しても責任を負いかねます。</p>
     </>
@@ -59,7 +80,6 @@ type Env = {
 };
 
 const checkEdible = async (name: string, apiKey: string): Promise<boolean> => {
-  return true;
   const openai = new OpenAI({ apiKey: apiKey });
   const chatres = await openai.chat.completions.create({
     messages: [
@@ -87,7 +107,6 @@ const generateImageUrl = async (
   name: string,
   apiKey: string
 ): Promise<string> => {
-  return "https://oaidalleapiprodscus.blob.core.windows.net/private/org-3PV7obYli2y7cULio6G5teVh/user-dHxXoeKsjwXXVGxG0LUjiZ8q/img-ce4XBol2Auy7N9KR6K2dYRN7.png?st=2023-12-21T14%3A17%3A07Z&se=2023-12-21T16%3A17%3A07Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-20T23%3A10%3A05Z&ske=2023-12-21T23%3A10%3A05Z&sks=b&skv=2021-08-06&sig=2pwhRyTYlBs9VHH4MJ%2BIUo0OgtMI%2BsAXwPpmRtDbDDs%3D";
   const openai = new OpenAI({ apiKey: apiKey });
   const res = await openai.images.generate({
     model: "dall-e-3",
